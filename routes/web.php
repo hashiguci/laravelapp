faeb8222caf3fe689fa2543138da80b3f72d9499
hashiguci <pf.y.hashiguchi@gmail.com>
2023-10-20 17:13:48 +0900

    2-1  ルーティング 完了まで

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
+| routes are loaded by the RouteServiceProvi{der and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
 */
@@ -21,6 +21,28 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('hellow/{msg?}', function ($msg = 'no message.') {
+
+    $html = <<<EOF
+<html>
+<head>
+<title>Hellow</title>
+<style>
+body { font-size:16pt; color:#999; }
+h1 { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
+</style>
+</head>
+<body>
+    <h1>Hellow</h1>
+    <p>{$msg}</p>
+    <p>これは、サンプルで作ったページです。</p>
+</body>
+</html>
+EOF;
+
+    return $html;
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
