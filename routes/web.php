cb354d3fedf050b17b9407be47bacab97795d45d
hashiguci <pf.y.hashiguchi@gmail.com>
2023-11-01 15:22:30 +0900

    3-1 コントローラでテンプレートを使う

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('hello', function() {
-    return view('hello.index');
-});
+Route::get('hello', [HelloController::class, 'index']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
