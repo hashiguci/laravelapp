2afb90b69d491914150ca3913fb337882ff335b9
hashiguci <pf.y.hashiguchi@gmail.com>
2023-11-07 12:21:50 +0900

    3-3 @ifディレクティブ

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -17,7 +17,11 @@
 </head>
 <body>
     <h1>Blade/Index</h1>
-    <p>{{$msg}}</p>
+    @if ($msg != '')
+    <p>こんいちは、{{$msg}}さん。</p>
+    @else
+    <p>何か書いてください。</p>
+    @endif
     <form method="POST" action="/hello">
         @csrf
         <input type="text" name="msg">
