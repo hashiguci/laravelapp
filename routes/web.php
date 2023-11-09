edfad7c078f6b9409b805dd6f396fad107d81b2c
hashiguci <pf.y.hashiguchi@gmail.com>
2023-11-09 15:44:35 +0900

    4-1 ミドルウェアのグループ

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('hello', [HelloController::class, 'index']);
+Route::get('hello', [HelloController::class, 'index'])->middleware('helo');
 Route::post('hello', [HelloController::class, 'post']);
 
 Route::middleware('auth')->group(function () {
