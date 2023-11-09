1437ba20ad6b90c9054636b0dfed2113d8bca13a
hashiguci <pf.y.hashiguchi@gmail.com>
2023-11-09 16:49:13 +0900

    4-2 フィールドごとにエラーを表示

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,36 +9,51 @@
 
 @section('content')
     <p>{{$msg}}</p>
-
     @if (count($errors) > 0)
-    <div>.
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
+    <p>入力に問題があります。再入力してください。</p>
     @endif
 
     <form action="/hello" method="post">
     <table>
         @csrf
+
+        @if ($errors->has('name'))
+            <tr>
+                <th>ERROR</th>
+                <td>{{ $errors->first('name') }}</td>
+            </tr>
+        @endif
         <tr>
             <th>name: </th>
             <td>
                 <input type="text" name="name" value="{{ old('name') }}">
             </td>
         </tr>
+
+        @if ($errors->has('mail'))
+            <tr>
+                <th>ERROR</th>
+                <td>{{ $errors->first('mail') }}</td>
+            </tr>
+        @endif
         <tr>
             <th>mail: </th>
             <td>
                 <input type="text" name="mail" value="{{ old('mail') }}">
             </td>
         </tr>
+
+        @if ($errors->has('age'))
+        {{-- $errors->get('項目名')とすれば指定した項目で発生してるすべてのエラーメッセージを取り出すことができる（配列） --}}
+            <tr>
+                <th>ERROR</th>
+                <td>{{ $errors->first('age') }}</td>
+            </tr>
+        @endif
         <tr>
             <th>age: </th>
             <td>
-                <input type="text" name="age" value="{{ old }}">
+                <input type="text" name="age" value="{{ old('age') }}">
             </td>
         </tr>
         <tr>
