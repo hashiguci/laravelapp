4e54eb2ab64ea7188717afb7237f3f1a0969681f
hashiguci <pf.y.hashiguchi@gmail.com>
2023-11-10 16:12:44 +0900

    4-3 バリデーションルールを作成する

---
+++ app/Rules/Myrule.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+
+
+class Myrule implements ValidationRule
+{
+    /**
+     * 
+     */
+    public function __construct(private int $n)
+    {
+        $this->n = $n;
+    }
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if ($value % $this->n != 0) {
+            $fail("{$this->n}で割り切れる値が必要です。");
+        }
+    }
+}
