4172bfbcb6b410f9b8ffd0c96cd5d69c68efd8f3
hashiguci <pf.y.hashiguchi@gmail.com>
2023-12-22 16:29:36 +0900

    5-2 DB::updateによる更新

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,8 @@ Route::get('hello', [HelloController::class, 'index']);
 Route::post('hello', [HelloController::class, 'post']);
 Route::get('hello/add', [HelloController::class, 'add']);
 Route::post('hello/add', [HelloController::class, 'create']);
+Route::get('hello/edit', [HelloController::class, 'edit']);
+Route::post('hello/edit', [HelloController::class, 'update']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
